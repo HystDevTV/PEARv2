3808ceb6af0bcce4480b27e79e8977f4c496d42a
HystDevTV <jan.p.e.steinert@web.de>
2025-07-25 11:30:30 +0200

    test cloudia

--- test_cloudia_gcp.py
+++ test_cloudia_gcp.py
@@ -2,7 +2,7 @@ from google.cloud import logging_v2
 from google.auth.exceptions import DefaultCredentialsError
 
 try:
-    client = logging_v2.LoggingServiceV2Client()
+    client = logging_v2.Client()
     print("✅ CloudIA ist verbunden.")
 except DefaultCredentialsError as e:
     print("❌ Fehler mit den Credentials:", e)
